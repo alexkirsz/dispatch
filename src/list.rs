86fae9a587983cb9d1b6a791b81ea8c0ed49f947
Alex Kirszenberg <alex@kirszenberg.com>
2023-05-06 21:17:19 +0200

    Build, test, and lint workflows (#18)

--- src/list.rs
+++ src/list.rs
@@ -1,6 +1,8 @@
+use std::net::IpAddr;
+
 use crate::net::bind_socket;
+use network_interface::{NetworkInterface, NetworkInterfaceConfig};
 use owo_colors::OwoColorize;
-use pnet_datalink::interfaces;
 use term_table::{
     row::Row,
     table_cell::{Alignment, TableCell},
@@ -9,33 +11,35 @@ use term_table::{
 
 pub fn list() {
     let mut table = Table::new();
-    table.max_column_width = 40;
+    table.max_column_width = 41;
     table.style = TableStyle::extended();
 
-    for interface in interfaces()
+    for interface in NetworkInterface::show()
+        .expect("failed to retrieve network interfaces")
         .into_iter()
-        .filter(|interface| !interface.is_loopback())
-        .filter(|interface| !interface.ips.is_empty())
+        .filter(|interface| !interface.addr.is_empty())
     {
-        let ips = {
-            let mut ips: Vec<_> = interface
-                .ips
+        let addrs = {
+            let mut addrs: Vec<_> = interface
+                .addr
                 .into_iter()
-                .map(|ip| ip.ip())
-                .filter(|ip| bind_socket(*ip).is_ok())
+                .map(|addr| addr.ip())
+                .filter(|addr| !is_local_address(addr))
+                .filter(|addr| bind_socket(*addr).is_ok())
                 .collect();
-            ips.sort_by_key(|ip| ip.is_ipv6());
-            ips
+            addrs.sort_by_key(|addr| addr.is_ipv6());
+            addrs
         };
 
-        if ips.is_empty() {
+        if addrs.is_empty() {
             continue;
         }
 
         table.add_row(Row::new(vec![
             TableCell::new_with_alignment(interface.name.bold(), 1, Alignment::Right),
             TableCell::new_with_alignment(
-                ips.iter()
+                addrs
+                    .iter()
                     .map(ToString::to_string)
                     .collect::<Vec<_>>()
                     .join("\n"),
@@ -46,4 +50,26 @@ pub fn list() {
     }
 
     println!("{}", table.render());
+}
+
+fn is_local_address(addr: &IpAddr) -> bool {
+    if addr.is_loopback() {
+        return true;
+    }
+
+    match addr {
+        IpAddr::V4(ip) => {
+            if ip.is_link_local() {
+                return true;
+            }
+        }
+        IpAddr::V6(ip) => {
+            // Check for link-local (fe80::/10)
+            if ip.segments()[0] & 0xffc0 == 0xfe80 {
+                return true;
+            }
+        }
+    }
+
+    false
 }
